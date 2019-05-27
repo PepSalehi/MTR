d55ae0deee13d38d6a1fd186a7cc7882c77f3ea9
Peyman Noursalehi <p.noursalehi@gmail.com>
2019-05-27 19:30:57 -0400

    trying a completely different fix to the path problem

--- inst/shinyApp/app.r
+++ inst/shinyApp/app.r
@@ -1,1 +1,8 @@
-DemandPackage::launchApp()
+appDir <- system.file("MTRdemand", package = "MTRdemand")
+if (appDir == "") {
+  stop("Could not find MTRdemand Try re-installing `MTRdemand`.", call. = FALSE)
+}
+
+shiny::runApp(appDir, display.mode = "normal")
+
+# DemandPackage::launchApp()
